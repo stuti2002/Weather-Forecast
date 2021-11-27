50f202dc64bec3d3acb93a1d44bcfc0fd5d24432
Stuti <stutijha34@gmail.com>
2021-11-27 17:53:20 +0530

    deploy weatherapp

---
+++ weatherapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import urllib.request,json
+
+# Create your views here.
+def index(request):
+    if request.method=='POST':
+        city=request.POST['city']
+        API=urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=f6d1137aa492f15ecfebda9cf0c0e787')
+        jsondata=json.load(API)
+        data={
+            "countrycode":str(jsondata['sys']['country']),
+            "coordinate":str(jsondata['coord']['lon'])+","+
+            str(jsondata['coord']['lat']),
+            "temp":str(jsondata['main']['temp']),
+            "pressure":str(jsondata['main']['pressure']),
+            "humidity":str(jsondata['main']['humidity']),
+            }   
+    else:
+        city=''
+        data={}
+
+    return render(request,'index.html',{'city':city,'data':data})
